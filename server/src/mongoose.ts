da95457071eb605ccb0377170e7af2a57ddd82ce
Hanzla Mateen <hanzlamateen@live.com>
2021-10-17 22:52:35 +0500

    Updated cert file path

--- server/src/mongoose.ts
+++ server/src/mongoose.ts
@@ -4,19 +4,16 @@ import { Application } from "./declarations";
 import logger from "./logger";
 
 export default function (app: Application): void {
-  const certFile = "ca-certificate.crt";
+  const certFile = "./ca-certificate.crt";
   if (process.env.CA_CERT && fs.existsSync(certFile) === false) {
     fs.writeFileSync(certFile, process.env.CA_CERT);
   }
-
-  logger.info(process.env.CA_CERT);
-  logger.info(app.get("mongodb"));
-  logger.info(certFile);
 
   mongoose
     .connect(app.get("mongodb"), {
       useCreateIndex: true,
       useNewUrlParser: true,
+      tlsCAFile: process.env.CA_CERT ? certFile : undefined,
     })
     .catch((err) => {
       logger.error(err);
